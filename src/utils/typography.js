f9c9c08127f06e659a36fdef85c83ff96e709586
Roger Guasch <roger.guasch.rion@gmail.com>
2021-01-14 17:25:24 +0100

    resolve conflicts

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -4,8 +4,4 @@ import fairyGateTheme from "typography-theme-fairy-gates";
 const typography = new Typography(fairyGateTheme);
 
 export const { scale, rhythm, options } = typography;
-<<<<<<< HEAD
-
-=======
->>>>>>> 1f60a317e9496c442a7041e257d7913a424b5266
 export default typography;
